3e281cbed87447b7c8b6eef1fed2655b51e3e901
Anbessaa <161745072+Anbessaa@users.noreply.github.com>
2024-07-10 17:11:18 +0500

    Update catsbot.py

--- catsbot.py
+++ catsbot.py
@@ -1,17 +1,28 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
-TOKEN = '7497254218:AAE2YfsgK1Ir0kJ0N4KwlSDmNjVzajxa5YQ'
+TOKEN = '7497254218:AAE2YfsgK1Ir0kJ0N4KwlSDmNjVzajxa5YQ'  # Replace with your real token
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    username = message.from_user.username
+    is_premium = message.from_user.is_premium  # This attribute needs checking if your Telegram API version supports it.
+    
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("Let's go", web_app=WebAppInfo(url='https://anbessaa.github.io/catsbot/')))
-    bot.reply_to(message, "How cool is your Telegram profile? Check your rating and receive rewards.", reply_markup=markup)
+    
+    welcome_message = f"Hello, {username}!\n"
+    if is_premium:
+        welcome_message += "You have Telegram Premium!\n"
+    else:
+        welcome_message += "You don't have Telegram Premium.\n"
+    welcome_message += "How cool is your Telegram profile? Check your rating and receive rewards."
+    
+    bot.reply_to(message, welcome_message, reply_markup=markup)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "Sorry, I can only process commands.")
+@bot.message_handler(commands=['invite'])
+def invite_friends(message):
+    bot.reply_to(message, "Invite your friends to join us! https://t.me/blackcat_community")
 
-bot.polling()
+bot.polling(none_stop=True)
